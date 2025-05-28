3b01719ebbf3f6f0afd8f56c635e6f99b1ddb63f
Meet-Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-05-28 10:31:43 +0530

    Time: 15 ms (40.43%) | Memory: 11.2 MB (36.35%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -4,12 +4,12 @@ public:
         vector<int> lastSeen(3, -1);
         int ans = 0;
 
-        for(int i = 0; i < s.size(); i++) {
+        for(int i = s.size()-1; i >= 0; i--) {
             int ch = s[i] - 'a';
             lastSeen[ch] = i;
 
             if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
-                ans = ans + min(lastSeen[0], min(lastSeen[1], lastSeen[2])) + 1;
+                ans = ans + (s.size() - max(lastSeen[0], max(lastSeen[1], lastSeen[2])));
             }
         }
 
