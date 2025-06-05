d074ce7d0328af63a3d1625cb9eb81e9b0a9ebd1
Meet-Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-06-06 00:16:43 +0530

    Time: 2 ms (26.25%) | Memory: 13.9 MB (5.95%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -7,17 +7,13 @@ public:
             if (c != ']') {
                 st.push(c);
             } else {
-                // Step 1: Get the string inside the brackets
                 string decodedStr = "";
                 while (!st.empty() && st.top() != '[') {
                     decodedStr = st.top() + decodedStr;
                     st.pop();
                 }
-
-                // Remove the '['
                 st.pop();
 
-                // Step 2: Get the number (could be more than 1 digit)
                 string numStr = "";
                 while (!st.empty() && isdigit(st.top())) {
                     numStr = st.top() + numStr;
@@ -26,20 +22,17 @@ public:
 
                 int count = stoi(numStr);
 
-                // Step 3: Repeat the decodedStr 'count' times
                 string repeated = "";
                 for (int i = 0; i < count; ++i) {
                     repeated += decodedStr;
                 }
 
-                // Step 4: Push repeated characters back to the stack
                 for (char ch : repeated) {
                     st.push(ch);
                 }
             }
         }
 
-        // Final step: Build the result from stack
         string result = "";
         while (!st.empty()) {
             result = st.top() + result;
