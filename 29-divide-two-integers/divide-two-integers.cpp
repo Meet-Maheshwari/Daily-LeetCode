dad812bc969d331e0185f7cb7b3e725640801c48
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-08-21 23:53:24 +0530

    Time: 2 ms (11.41%) | Memory: 8.7 MB (37.22%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(1LL*dividend == -2147483648 && divisor == -1) {
+            return 2147483647;
+        }
+        return (int)((long long)dividend / divisor);
+    }
+};
