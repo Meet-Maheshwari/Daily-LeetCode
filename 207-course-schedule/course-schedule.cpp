63768f8b3999c75b37ca163470de347400c8251b
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-09-18 16:22:07 +0530

    Time: 81 ms (5.00%) | Memory: 17.8 MB (99.53%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    bool findCycle(list<int>* l, int src, vector<bool>& vis, vector<bool>& recPath) {
+    bool findCycle(int src, vector<bool>& recPath, vector<bool>& vis, vector<vector<int>>& graph) {
         vis[src] = true;
         recPath[src] = true;
+        int V = graph.size();
+        
+        for(int i = 0; i < V; i++) {
+            int u = graph[i][1];
+            int v = graph[i][0];
 
-        list<int> neighbors = l[src];
-        for(int v : neighbors) {
-            if(!vis[v]) {
-                if(findCycle(l, v, vis, recPath)) {
-                    return true;
-                }
-            } else {
-                if(recPath[v]) {
-                    return true;
+            if(u == src) {
+                if(!vis[v]) {
+                    if(findCycle(v, recPath, vis, graph)) {
+                        return true;
+                    }
+                } else{
+                    if(recPath[v]) {
+                        return true;
+                    }
                 }
             }
         }
@@ -20,23 +25,14 @@ public:
         recPath[src] = false;
         return false;
     }
-
+    
     bool canFinish(int numCourses, vector<vector<int>>& graph) {
         int V = numCourses;
         vector<bool> vis (V, false);
-        vector<bool> recPath(V, false);
-
-        list<int>*l = new list<int>[V];
-
-        for(int i = 0; i < graph.size(); i++) {
-            l[graph[i][1]].push_back(graph[i][0]);
-        }
-
+        vector<bool> recPath (V, false);
         for(int i = 0; i < V; i++) {
-            if(!vis[i]) {
-                if(findCycle(l, i, vis, recPath)) {
-                    return false;
-                }
+            if(findCycle(i, recPath, vis, graph)) {
+                return false;
             }
         }
         return true;
