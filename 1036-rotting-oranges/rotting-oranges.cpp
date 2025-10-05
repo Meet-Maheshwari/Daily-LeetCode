a1c21ffaad9e6f570a22896d8b48da6972f4303d
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-10-05 14:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (88.15%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -5,50 +5,49 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        queue<pair<pair<int, int>, int>> Q;
+        queue<pair<int, pair<int, int>>> Q;
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 2) {
-                    Q.push(make_pair(make_pair(i, j), 0));
+                    Q.push({0, {i, j}});
                 }
-            }
+            } 
         }
 
-        vector<int> nrow = {-1, 0, 1, 0};
-        vector<int> ncol = {0, 1, 0, -1};
+        vector<int> nrow = {0, 1, 0, -1};
+        vector<int> ncol = {1, 0, -1, 0};//right, down, left, top
 
-        int tm = 0;
+        int minTime = 0;
         while(!Q.empty()) {
-            pair<pair<int, int>, int> curr = Q.front();
+            pair<int, pair<int, int>> curr = Q.front();
             Q.pop();
 
-            int row = curr.first.first;
-            int col = curr.first.second;
-            int t = curr.second;
-            tm = max(tm, t);
+            int tm = curr.first;
+            minTime = max(minTime, tm);
+            int crow = curr.second.first;
+            int ccol = curr.second.second;
 
             for(int i = 0; i < 4; i++) {
-                int n_row = row + nrow[i];
-                int n_col = col + ncol[i];
+                int row = crow + nrow[i];
+                int col = ccol + ncol[i];
 
-                if(n_row >= 0 && n_row < n && n_col >= 0 && n_col < m && grid[n_row][n_col] == 1) {
-                    grid[n_row][n_col] = 2;
-                    Q.push(make_pair(make_pair(n_row, n_col), t+1));
+                if(row < n && row >= 0 && col < m && col >= 0 && grid[row][col] == 1) {
+                    grid[row][col] = 2;
+                    Q.push({tm+1, {row, col}});
                 }
             }
         }
 
-        bool remaining = false;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 1) {
-                    remaining = true;
+                    minTime = -1;
                     break;
-                }
-            }
+                } 
+            } 
         }
 
-        return remaining == true ? -1 : tm;
+        return minTime;   
     }
 };
