b22c6234b2b631cb288af28a1cce25e9d94f8ab9
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-10-08 10:54:15 +0530

    Time: 1 ms (36.13%) | Memory: 13.1 MB (60.61%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     long long countSubstrings(string s, char c) {
-        unordered_set<int> set;
+        long long lastS = 0;
         long long count = 0;
         int n = s.size();
 
         for(int i = 0; i < n; i++) {
             if(s[i] == c) {
-                count += set.size() + 1;
-                set.insert(i);
+                count += lastS + 1;
+                lastS++;
             }
         }
 
