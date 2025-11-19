dadad5817179137245a3b2c62d7b1e8f8064d3bf
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-11-19 11:07:11 +0530

    Time: 659 ms (5.00%) | Memory: 15.1 MB (13.96%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,44 +1,59 @@
 class Solution {
 public:
-    // int minOperations(int n, int m, string w1, string w2, vector<vector<int>>& dp) {
-    //     if(n == 0) return m;
-    //     if(m == 0) return n;
+    //top-bottom memoization
+    int calculate(string& word1, string& word2, int n, int m, vector<vector<int>>& dp) {
+        if(m == 0) return dp[n][m] = n;
+        if(n == 0) return dp[n][m] = m;
 
-    //     if(dp[n][m] != -1) {
-    //         return dp[n][m];
-    //     }
+        if(dp[n][m] != -1) return dp[n][m];
 
-    //     if(w1[n-1] == w2[m-1]) {
-    //         dp[n][m] = 0 + minOperations(n-1, m-1, w1, w2, dp);
-    //     } else {
-    //         dp[n][m] = 1 + min(minOperations(n-1, m-1, w1, w2, dp), min(minOperations(n-1, m, w1, w2, dp), minOperations(n, m-1, w1, w2, dp)));
-    //     }
+        if(word1[n-1] == word2[m-1]) {
+            dp[n][m] = 0 + calculate(word1, word2, n-1, m-1, dp);
+        } else {
+            int first = 1 + calculate(word1, word2, n, m-1, dp);
+            int second = 1 + calculate(word1, word2, n-1, m, dp);
+            int third = 1 + calculate(word1, word2, n-1, m-1, dp);
 
-    //     return dp[n][m];
-    // };
+            dp[n][m] = min(first, min(second, third));
+        }
+
+        return dp[n][m];
+    }
 
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<int> prev(m+1, 0);
 
-        for(int j = 1; j < m+1; j++) {
-            prev[j] = j;
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
+        // calculate(word1, word2, n, m, dp);
+
+        for(int i = 1; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 1; j < m + 1; j++) {
+            dp[0][j] = j;
         }
 
         for(int i = 1; i < n+1; i++) {
-            vector<int> dp(m+1, 0);
-            dp[0] = i;
             for(int j = 1; j < m+1; j++) {
                 if(word1[i-1] == word2[j-1]) {
-                    dp[j] = 0 + prev[j-1];
+                    dp[i][j] = 0 + dp[i-1][j-1];
                 } else {
-                    dp[j] = 1 + min(prev[j-1], min(prev[j], dp[j-1]));
+                    long long first = 1 + dp[i-1][j];
+                    long long second = 1 + dp[i][j-1];
+                    long long third = 1 + dp[i-1][j-1];
+                    dp[i][j] = min(first, min(second, third));
                 }
             }
-            prev = dp;
         }
 
-        return prev[m];
+        for(int i = 0; i < n+1; i++) {
+            for(int j = 0; j < m+1; j++) {
+                cout << dp[i][j] << " ";
+            }
+            cout << endl;
+        }
+        
+        return dp[n][m];
     }
 };
