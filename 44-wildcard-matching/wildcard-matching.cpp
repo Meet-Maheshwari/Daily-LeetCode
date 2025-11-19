cf973ba8b8d9cd0a3f3ae779daef58b4ac96893f
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-11-19 13:53:47 +0530

    Time: 68 ms (14.50%) | Memory: 54.7 MB (9.49%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -35,7 +35,35 @@ public:
         int m = p.size();
         vector<vector<int>> dp (n+1, vector<int>(m+1, -1));
 
-        solve(t, p, n, m, dp);
+        dp[0][0] = 1;
+
+        for(int j = 1; j < m+1; j++) {
+            if(p[j-1] == '*' && dp[0][j-1] == 1) {
+                dp[0][j] = 1;
+            } else {
+                dp[0][j] = 0;
+            }
+        }
+
+        for(int i = 1; i < n+1; i++) {
+            dp[i][0] = 0;
+        }
+
+
+        for(int i = 1; i < n+1; i++) {
+            for(int j = 1; j < m+1; j++) {
+                if(p[j-1] == t[i-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if(p[j-1] == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                } else if(p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
         return dp[n][m];
     }
 };
