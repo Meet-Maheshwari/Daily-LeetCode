56b23c55babf7e652862751b1feec5010087a62a
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2026-01-01 20:27:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (26.58%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    set<vector<int>> s;
+    vector<vector<int>> mat;
 
-    void findSubsets(int i, vector<vector<int>>& ans, vector<int>& subsets, vector<int>& nums, int n) {
-        if(i == n) {
-            if(s.find(subsets) == s.end()) {
-                s.insert(subsets);
-                ans.push_back(subsets);
-            }
+    void rec(vector<int>& nums, vector<int>& output, int idx) {
+        if(idx == nums.size()) {
+            mat.push_back(output);
             return;
         }
 
-        //include
-        subsets.push_back(nums[i]);
-        findSubsets(i+1, ans, subsets, nums, n);
-        subsets.pop_back();
-        //exclude;
-        findSubsets(i+1, ans, subsets, nums, n);
-    } 
+        // YES choice
+        output.push_back(nums[idx]);
+        rec(nums, output, idx + 1);
+        output.pop_back(); // backtracking
+
+        // ❌ DUPLICATE ISSUE FIXED HERE
+        // skip all duplicates
+        int nextIdx = idx + 1;
+        while(nextIdx < nums.size() && nums[nextIdx] == nums[idx]) {
+            nextIdx++;
+        }
+
+        // NO choice
+        rec(nums, output, nextIdx);
+    }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        vector<int> subsets;
-
-        findSubsets(0, ans, subsets, nums, n);
-        return ans;
+        sort(nums.begin(), nums.end());   // ❌ REQUIRED to handle duplicates
+        vector<int> output;
+        rec(nums, output, 0);
+        return mat;
     }
 };
