51e9682ce1aefa79bcc90602edff2776fc9ea09b
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2026-01-09 22:54:55 +0530

    Time: 23 ms (50.79%) | Memory: 17.5 MB (54.47%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target){
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < n - 3; i++) {
+
+        for(int i = 0; i < n; i++) {
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            for(int j = i+1; j < n-2; ) {
-                int p = j+1, q = n-1;
+
+            for(int j = i+1; j < n;) {
+                int p = j+1;
+                int q = n-1;
 
                 while(p < q) {
-                    long long sum = 1LL * nums[i] + nums[j] + nums[p] + nums[q];
+                    long long sum = (long long)nums[i] + nums[j] + nums[p] + nums[q];
 
-                    if(sum > target) {
-                        q--;
+                    if(sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[p], nums[q]});
+                        p++, q--;
+                        while(p < q && nums[p] == nums[p-1]) p++; 
                     } else if(sum < target) {
                         p++;
-                    } else{
-                        ans.push_back({nums[i], nums[j], nums[p], nums[q]});
-                        p++; q--;
-
-                        while(p < q && nums[q] == nums[q+1]) q--;
+                    } else {
+                        q--;
                     }
                 }
-
-                ++j;
-                while(j < n-2 && nums[j] == nums[j-1]) j++;
+                j++;
+                while(j < n && nums[j] == nums[j-1]) j++;
             }
-        }
-
+        }       
         return ans;
     }
 };
