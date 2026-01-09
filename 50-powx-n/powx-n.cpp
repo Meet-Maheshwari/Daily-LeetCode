37b94dde1bbb35c0d638a57459b1dc1d0e31f29e
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2026-01-09 17:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (42.69%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     double findPow(double x, long long n) {
-        if (n <= 0) {
-            return 1;
-        }
+        if(n == 0) return 1;
 
+        double halfPow = 0;
         if(n % 2 == 0) {
-            double halfPow = findPow(x, n/2);
-            return (double)halfPow * (double)halfPow;
-        } else {
-            double halfPow = findPow(x, n/2);
-            return x * (double)halfPow * (double)halfPow;
-        }
+            halfPow = findPow(x, n/2);
+            return halfPow * halfPow;
+        } 
+        
+        halfPow = findPow(x, n/2);
+        return x * halfPow * halfPow;
     }
 
     double myPow(double x, int n) {
-        long long N = n;
-        if(N < 0) {
-            N = abs(N - 0);
-            return 1 / findPow(x, N);
+        if (n == 0) return 1;
+        if (n < 0) {
+            long long N = -(long long)n;
+            return 1/(findPow(x, N));
         }
-
-        return findPow(x, N);
+        return findPow(x, n);
     }
 };
